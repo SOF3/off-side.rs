481b997334de098cee6c331b8b64975d0f6a5c26
chankyin <chankyin1970@gmail.com>
2019-06-26 12:39:46 +0800

    Added license headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Copyright (C) 2019 chankyin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 extern crate indent_stack;
 extern crate proc_macro;
 extern crate proc_macro2;
@@ -20,6 +34,5 @@ fn off_side_impl(input: TokenStream1) -> syn::Result<TokenStream1> {
     let mut converter = Converter::default();
     converter.iterate(TokenStream2::from(input).into_iter())?;
     let stream = converter.collect()?;
-    dbg!(&stream.to_string());
     Ok(stream.into())
 }
